25fbcbb2929da056cd8bdb348e0f22e882519182
Michal Turczyn <turek1992@o2.pl>
2023-06-27 10:58:00 +0200

    Added more notes about local BE and TanStack package.

--- src/tanStackQuery/ExampleApiData.tsx
+++ src/tanStackQuery/ExampleApiData.tsx
@@ -3,25 +3,45 @@ import {
   QueryClientProvider,
   useQuery,
 } from '@tanstack/react-query'
-import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import { useState } from 'react'
 
 const queryClient = new QueryClient()
 
+const localEndpoint = 'http://localhost:5000/examplewithdelay'
+const githubEndpoint = 'https://api.github.com/repos/tannerlinsley/react-query'
+
 export default function TanStackApp() {
+  const [withLocalEndpoint, setWithLocalEndpoint] = useState(false)
+
   return (
     <div>
       <QueryClientProvider client={queryClient}>
         <p>
           Below is example of fetching data from API with TanStack npm package.
         </p>
-        <p>To run locally some sample API, use csharprepl for that:</p>
-        <ReactMarkdown>{`\`\`\`
-csharprepl --framework Microsoft.AspNetCore.App
-\`\`\``}</ReactMarkdown>
+        <p>
+          To run locally some sample API, use csharprepl for that (.NET is
+          required, dotnet CLI to install csharprepl if needed):
+        </p>
+        <pre
+          style={{
+            background: 'lightgrey',
+            padding: '1rem',
+            fontFamily: 'monospace',
+          }}
+        >
+          <code>{`csharprepl --framework Microsoft.AspNetCore.App`}</code>
+        </pre>
         <p>And paste code below</p>
-        <ReactMarkdown>
-          {`\`\`\`
-using Microsoft.Extensions.DependencyInjection;
+        <pre
+          style={{
+            background: 'lightgrey',
+            padding: '1rem',
+            fontFamily: 'monospace',
+          }}
+        >
+          <code>
+            {`using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using System.Text.Json;
 
@@ -35,23 +55,54 @@ var app = builder.Build();
 
 app.UseCors();
 
-app.MapGet("example", () => JsonSerializer.Serialize(new { name = "Michal" }));
+app.MapGet("example", () => 
+{
+    return JsonSerializer.Serialize(new { name = "Michal", description = "Michal is awesome", subscribers_count = 1000, stargazers_count = 999, forks_count = -3 });
+});
 
-app.Run();
-\`\`\``}
-        </ReactMarkdown>
-        <Example />
+app.MapGet("examplewithdelay", () => 
+{
+    // Add artificial delay to imitate BE processing if needed.
+    Task.Delay(1000).GetAwaiter().GetResult();
+    return JsonSerializer.Serialize(new { name = "Delayed Michal", description = "Result with artificial slowdown", subscribers_count = 1000, stargazers_count = 999, forks_count = -3 });
+});
+
+app.Run();`}
+          </code>
+        </pre>
+        <label>
+          Use local endpoint
+          <input
+            type='checkbox'
+            checked={withLocalEndpoint}
+            onChange={(e: any) => setWithLocalEndpoint(e.target.checked)}
+          />
+        </label>
+        <table>
+          <tr>
+            <td>Local endpoint</td>
+            <td>
+              <b>{localEndpoint}</b>
+            </td>
+          </tr>
+          <tr>
+            <td>Github endpoint</td>
+            <td>
+              <b>{githubEndpoint}</b>
+            </td>
+          </tr>
+        </table>
+        <Example url={withLocalEndpoint ? localEndpoint : githubEndpoint} />
       </QueryClientProvider>
     </div>
   )
 }
 
-function Example() {
+function Example({ url }: any) {
   const { isLoading, error, data } = useQuery<any>({
-    queryKey: ['repoData'],
+    queryKey: [url],
     queryFn: async () => {
-      const data = await fetch('http://localhost:5000/example')
-      //   const data = await fetch('https://api.github.com/repos/mturczyn/')
+      const data = await fetch(url)
       return await data.json()
     },
   })
